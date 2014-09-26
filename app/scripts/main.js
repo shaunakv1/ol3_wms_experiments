42a0f5d45844f7db4ac65e5bca027246ebd8cd4d
shaunakv1 <shaunakv1@gmail.com>
2014-09-25 19:25:17 -0500

    added water level slider and updating the WMS image dynamically.. so it shows growing water

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,6 +3,18 @@ $(function () {
     	html: 'Tiles &copy; <a href="http://services.arcgisonline.com/ArcGIS/' + 'rest/services/World_Topo_Map/MapServer">ArcGIS</a>'
 	});
 
+	var wmsSource = new ol.source.TileWMS(({
+	            url: 'http://floodwatch.houstontranstar.org/geoserver/cite/wms',
+	            params: {
+	                'LAYERS': 'cite:lidar_wsg841',
+	                'TILED': true,
+	                'SLD_BODY':renderStyle(50),
+	                'SERVICE':'WMS',
+					'VERSION':'1.1.1'
+	            },
+	            serverType: 'geoserver'
+	        }));
+
 	var layers = [
 	    new ol.layer.Tile({
             source: new ol.source.XYZ({
@@ -13,17 +25,7 @@ $(function () {
 	    new ol.layer.Tile({
 	        opacity: 0.4,
 	        extent: [-13884991, 2870341, -7455066, 6338219],
-	        source: new ol.source.TileWMS(({
-	            url: 'http://floodwatch.houstontranstar.org/geoserver/cite/wms',
-	            params: {
-	                'LAYERS': 'cite:lidar_wsg841',
-	                'TILED': true,
-	                'SLD_BODY':getSLD('cite:lidar_wsg841',50),
-	                'SERVICE':'WMS',
-					'VERSION':'1.1.1'
-	            },
-	            serverType: 'geoserver'
-	        }))
+	        source: wmsSource
 	    })
 	];
 	var map = new ol.Map({
@@ -34,9 +36,17 @@ $(function () {
 	        zoom: 10
 	    })
 	});
+
+	$('#waterlevel').on("change mousemove", function() {
+	    //$(this).next().html($(this).val());
+	    console.log($(this).val());
+	    wmsSource.updateParams({
+	    	'SLD_BODY':renderStyle($(this).val())
+	    });
+	});
 });
 
-function getSLD() {
+function renderStyle(waterLevel) {
    var sld = '<StyledLayerDescriptor version="1.0.0" xmlns="http://www.opengis.net/sld" xmlns:ogc="http://www.opengis.net/ogc" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.opengis.net/sld StyledLayerDescriptor.xsd">'+
    	'<NamedLayer>'+
    		'<Name>cite:lidar_wsg841</Name>'+
@@ -48,8 +58,12 @@ function getSLD() {
    						'<ColorMap type="ramp">'+
    							'<ColorMapEntry color="#EEBE2F" quantity="-50" opacity="0"/>'+
    							'<ColorMapEntry color="#0066FF" quantity="0" opacity="1"/>'+
-   							'<ColorMapEntry color="#A5ECEF" quantity="20" opacity="0" />'+
-   							'<ColorMapEntry color="#FFFFFF" quantity="100" opacity="0" />'+
+   							'<ColorMapEntry color="#A5ECEF" quantity="'+waterLevel+'" opacity="0" />'+
+   							
+   							// for solid color rendndering.. also change color ramp type to 'intervals'
+							// '<ColorMapEntry color="#EEBE2F" quantity="-50" opacity="0"/>'+
+   							// '<ColorMapEntry color="#0066FF" quantity="0" opacity="1"/>'+
+   							// '<ColorMapEntry color="#0066FF" quantity="'+waterLevel+'" opacity="1" />'+
 
    						'</ColorMap>'+
    					'</RasterSymbolizer>'+
